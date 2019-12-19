23a1502841073b1df759b3fade42346762faa6f9
Adrian Bączek <adrian@sharedmasters.com>
2019-12-19 12:46:54 +0100

    some securities for bad input data

--- src/InputDownloader.java
+++ src/InputDownloader.java
@@ -12,11 +12,30 @@ public class InputDownloader {
         double[][] distances = getDistancesForCitiesFromInput();
         ArrayList<String> names = getNamesForCitiesFromInput();
 
-        if (distances.length != names.size())
+        if (distances.length != names.size()) {
             throw new InvalidParameterException(
                     String.format("Podano złą liczbę miast w stosunku do matrycy sąsiedztwa. Podana ilość: %d, wymagana ilość: %d",
                             names.size(),
                             distances.length));
+        }
+
+        for (int i = 0; i < distances.length; i++) {
+            if (distances[i].length != names.size()) {
+                throw new InvalidParameterException(
+                        String.format("W wierszu %d podano za małą liczbę połączeń miast w stosunku do ich liczebności. Podana ilość: %d, wymagana ilość: %d",
+                                i + 1,
+                                distances[i].length,
+                                names.size()));
+            }
+            for (int j = 0; j < distances[i].length; j++) {
+                if (distances[i][j] < 0) {
+                    throw new InvalidParameterException(
+                            String.format("Podano ujemną wagę połączenia miast w wierszu: %d, kolumnie: %d",
+                                    i + 1,
+                                    j + 1));
+                }
+            }
+        }
 
         data = new Data(names, distances);
     }
@@ -37,7 +56,7 @@ public class InputDownloader {
     }
 
     private ArrayList<String> getNamesForCitiesFromInput() {
-        ArrayList<String> namesForCities = new ArrayList<>(Arrays.asList("Nowy Jork", "Hong Kong", "Piła", "Tokio", "Warszawa"));
+        ArrayList<String> namesForCities = new ArrayList<>(Arrays.asList("Nowy Jork", "Hong Kong", "Piła", "Tokio", "Warszawa", "Serock"));
         return namesForCities;
     }
 
